0b6323c8368f51114de772557b3c46c6335217e3
lidashuang <ldshuang@gmail.com>
2012-11-02 21:24:44 +0800

    add section controller

---
+++ app/controllers/section.rb
@@ -0,0 +1,31 @@
+Teaching.controllers :section do
+  # get :index, :map => "/foo/bar" do
+  #   session[:foo] = "bar"
+  #   render 'index'
+  # end
+
+  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get "/example" do
+  #   "Hello world!"
+  # end
+
+  get :index do
+    render 'section/index'
+  end
+
+  get :new do
+  end
+
+  post :create do
+  end
+
+end

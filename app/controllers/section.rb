bd34200ead803cdd2a0d14d256dc704efe870308
lidashuang <ldshuang@gmail.com>
2012-11-10 00:31:11 +0800

    views

--- app/controllers/section.rb
+++ app/controllers/section.rb
@@ -4,6 +4,7 @@ Teaching.controllers :section do
   end
 
   get :index, :with => :key do
+    @key = params[:key]
     @section = Section.find_by_key(params[:key])
     @categories = @section.categories
     render "#{params[:key]}_index"
